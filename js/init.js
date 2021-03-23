23a7d1b22ae3d28afa7c3a2b915750bea0072fd3
liz1808 <81264170+liz1808@users.noreply.github.com>
2021-03-23 16:31:34 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyBKXKRaQCWdfiSCZR39wunK0QApZMhgeqE",
-    authDomain: "pauten43.firebaseapp.com",
-    projectId: "pauten43",
-    storageBucket: "pauten43.appspot.com",
-    messagingSenderId: "167024006136",
-    appId: "1:167024006136:web:2766577637b6ab37c43c0d",
-    measurementId: "G-S4KD63PHHW"
+    apiKey: "AIzaSyDWm3V0PXbhTjjj8nxXJhhop_XpTCla2Yw",
+    authDomain: "autentifica41m.firebaseapp.com",
+    projectId: "autentifica41m",
+    storageBucket: "autentifica41m.appspot.com",
+    messagingSenderId: "325929054061",
+    appId: "1:325929054061:web:75c29977bc4be664a7f464",
+    measurementId: "G-K931GRE12V"
     });
