89bde02a03530d44bc94c7ef21fa1e7cd99bada9
liz1808 <81264170+liz1808@users.noreply.github.com>
2021-03-24 18:08:11 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,12 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDWm3V0PXbhTjjj8nxXJhhop_XpTCla2Yw",
-    authDomain: "autentifica41m.firebaseapp.com",
-    projectId: "autentifica41m",
-    storageBucket: "autentifica41m.appspot.com",
-    messagingSenderId: "325929054061",
-    appId: "1:325929054061:web:75c29977bc4be664a7f464",
-    measurementId: "G-K931GRE12V"
+    apiKey: "AIzaSyCjxIg1JvF1u-RNaUFcZ3DbUgVqNIYIIek",
+    authDomain: "chat41lamc.firebaseapp.com",
+    projectId: "chat41lamc",
+    storageBucket: "chat41lamc.appspot.com",
+    messagingSenderId: "470804825821",
+    appId: "1:470804825821:web:4eaefeffb35c528234bd34",
+    measurementId: "G-WFJFSXBG79"
+
     });
